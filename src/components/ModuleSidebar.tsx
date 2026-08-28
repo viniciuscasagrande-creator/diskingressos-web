114ab5ddc86817fa9b38e49f68e572ed8f40bb88
Vinicius <vinicius@diskingressos.com.br>
2026-08-28 08:47:35 -0300

    Update_menu_to_Todos_os_Eventos_and_remove_Hub_de_Eventos

--- src/components/ModuleSidebar.tsx
+++ src/components/ModuleSidebar.tsx
@@ -37,7 +37,7 @@ const financeAdvanced:Item[]=[
  {key:'finance-custom',label:'Pagam. Customizados',icon:Settings},{key:'finance-operators',label:'Operadoras de Cartão',icon:ShieldCheck},
 ]
 const eventMain:Item[]=[
- {key:'profile-dashboard',label:'Meu Dashboard',icon:LayoutDashboard}, {key:'events',label:'Hub de Eventos',icon:LayoutDashboard},{key:'operations',label:'Núcleo Operacional',icon:ChartNoAxesCombined},{key:'events',label:'Todos os Eventos',icon:Ticket},
+ {key:'profile-dashboard',label:'Meu Dashboard',icon:LayoutDashboard}, {key:'events',label:'Todos os Eventos',icon:Ticket},{key:'operations',label:'Núcleo Operacional',icon:ChartNoAxesCombined},
  {key:'new-event',label:'Novo Evento',icon:PlusSquare},{key:'lots',label:'Configurar Lotes',icon:SlidersHorizontal},
  {key:'participants',label:'Participantes',icon:Users},{key:'facial',label:'Status Faciais',icon:ScanFace},
 ]
